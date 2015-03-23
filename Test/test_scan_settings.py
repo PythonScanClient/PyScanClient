2dd48416c8d34f9958f5e8003ac05b54ea17f26c
kasemir <kasemirk@ornl.gov>
2015-03-23 16:48:37 -0400

    Scan settings: Initial wrapup, test example for custom settings

--- Test/test_scan_settings.py
+++ Test/test_scan_settings.py
@@ -6,6 +6,20 @@ Unit test of ScanSettings
 import unittest
 from scan.util.scan_settings import DeviceSettings, ScanSettings
 
+class MyScanSettings(ScanSettings):
+    def __init__(self):
+        super(MyScanSettings, self).__init__()
+        # Define special settings for some devices
+        self.defineDeviceClass("My:Lakeshore.*", completion=True, readback=False, timeout=300, tolerance=10)
+        self.defineDeviceClass("My:Motor.*", completion=True, readback=True, timeout=100)
+        
+    def getReadbackName(self, device_name):
+        # Motors use their *.RBV field for readback
+        if "Motor" in device_name:
+            return device_name + ".RBV"
+        return device_name
+
+
 class DeviceSettingsTest(unittest.TestCase):
     def testDeviceSettings(self):
         s = DeviceSettings("device", readback=False)
@@ -23,66 +37,81 @@ class DeviceSettingsTest(unittest.TestCase):
         self.assertEquals(s.getName(), "device")
         self.assertEquals(s.getReadback(), "other")
 
-    def testBasicScanSettings(self):
-        settings = ScanSettings()
-        
-        # Define special settings for some devices
-        settings.defineDeviceClass("My:Lakeshore.*", completion=True, readback=False, timeout=300)
+    def testDefaultSettings(self):
+        settings = MyScanSettings()
         
         # Check device that should have special settings
-        s = settings.getDeviceSettings("My:Lakeshore1")
+        s = settings.getDefaultSettings("My:Lakeshore1")
         print s
         self.assertEquals(s.getName(), "My:Lakeshore1")
         self.assertEquals(s.getCompletion(), True)
         self.assertEquals(s.getReadback(), None)
         self.assertEquals(s.getTimeout(), 300)
+        self.assertEquals(s.getTolerance(), 10)
 
         # Check device that should NOT have them
-        s = settings.getDeviceSettings("Your:Lakeshore1")
+        s = settings.getDefaultSettings("Your:Lakeshore1")
         print s
         self.assertEquals(s.getName(), "Your:Lakeshore1")
         self.assertEquals(s.getCompletion(), False)
-        self.assertEquals(s.getReadback(), "Your:Lakeshore1")
+        self.assertEquals(s.getReadback(), None)
+        
+        # 'Motor' that uses *.RBV for a readback
+        s = settings.getDefaultSettings("My:Motor:47")
+        print s
+        self.assertEquals(s.getName(), "My:Motor:47")
+        self.assertEquals(s.getCompletion(), True)
+        self.assertEquals(s.getReadback(), "My:Motor:47.RBV")
+        self.assertEquals(s.getTimeout(), 100)
+        self.assertEquals(s.getTolerance(), 0)
+
+    def testDeviceModifiers(self):
+        settings = MyScanSettings()
+        
+        spec = "My:Lakeshore1"
+        (s, p) = settings.parseDeviceSettings(spec)
+        print "%s -> %s" % (spec, s)
+        self.assertEquals(s.getName(), "My:Lakeshore1")
+        self.assertEquals(s.getCompletion(), True)
+        self.assertEquals(s.getReadback(), None)
+        self.assertEquals(s.getTimeout(), 300)
+        self.assertEquals(p, False)
+
+        spec = "+p My:Lakeshore1"
+        (s, p) = settings.parseDeviceSettings(spec)
+        self.assertEquals(p, True)
+
+        spec = "-c My:Lakeshore1"
+        (s, p) = settings.parseDeviceSettings(spec)
+        print "%s -> %s" % (spec, s)
+        self.assertEquals(s.getName(), "My:Lakeshore1")
+        self.assertEquals(s.getCompletion(), False)
+        self.assertEquals(s.getReadback(), None)
+
+        spec = "+p-c+r My:Lakeshore1"
+        (s, p) = settings.parseDeviceSettings(spec)
+        print "%s -> %s" % (spec, s)
+        self.assertEquals(s.getName(), "My:Lakeshore1")
+        self.assertEquals(s.getCompletion(), False)
+        self.assertEquals(s.getReadback(), "My:Lakeshore1")
+        self.assertEquals(p, True)
+
+        spec = "+pr My:Lakeshore1"
+        (s, p) = settings.parseDeviceSettings(spec)
+        print "%s -> %s" % (spec, s)
+        self.assertEquals(s.getName(), "My:Lakeshore1")
+        self.assertEquals(s.getReadback(), "My:Lakeshore1")
+        self.assertEquals(p, True)
+
+        spec = "+p-cr My:Motor:47"
+        (s, p) = settings.parseDeviceSettings(spec)
+        print "%s -> %s" % (spec, s)
+        self.assertEquals(s.getName(), "My:Motor:47")
+        self.assertEquals(s.getCompletion(), False)
+        self.assertEquals(s.getReadback(), None)
+        self.assertEquals(p, True)
 
 
- 
 if __name__ == "__main__":
     unittest.main()
-    settings = ScanSettings()
-
-    print settings.parseDeviceModifiers("plain_device")
-    print settings.parseDeviceModifiers("-r no_readback")
-    print settings.parseDeviceModifiers("+r with_readback")
-    print settings.parseDeviceModifiers("+c with_callback")
-    print settings.parseDeviceModifiers("+cr with_callback_and_readback")
-    print settings.parseDeviceModifiers("+rc with_callback_and_readback")
-    print settings.parseDeviceModifiers("+c +r with_callback_and_readback")
-    print settings.parseDeviceModifiers("+c+r with_callback_and_readback")
-    print settings.parseDeviceModifiers("+c +rp with_callback_and_readback_in_parallel")
-    print settings.parseDeviceModifiers("+p with:Mot:callback_and_readback_in_parallel")
-
-
-    print settings.parseDeviceModifiers("Other")
-    print settings.parseDevice("Other")
-
     
-    print "%-22s: %-20s %-35s %-10s %-10s %-10s" % ( "Device", "PV", "Readback", "Completion?", "Timeout", "Tolerance" )
-    print "---------------------------------------------------------------------------------------------------------------"
-
-    
-    print "Title PV: %s" % settings.title
-    
-    settings.device_settings['TestWithCallback'] = { 'callback': True, 'readback': 'Test', 'timeout': None }
-    settings.device_settings['TestWithoutReadback'] = { 'callback': False, 'readback': None, 'timeout': None }
-    for device in [ "BL99:Mot:Whatever", "Speed",
-                    "-r-c BL99:Mot:Whatever",
-                    "-c-r BL99:Mot:Whatever",
-                    "BL99:Other", "+c BL99:Other", "-r BL99:Other", "-r+r BL99:Other",
-                    "TestWithCallback", "TestWithoutReadback"
-                  ]:
-                    
-        (pv, readback, completion, timeout, tolerance) = settings.parseDevice(device)
-        if readback is None:
-            readback = "-None-"
-        print "%-22s: %-20s %-35s %-10s %-10s %-10s" % (device, pv, readback, completion, timeout, tolerance)
-
--- scan/util/scan_settings.py
+++ scan/util/scan_settings.py
@@ -1,6 +1,9 @@
 """
 Scan settings
 
+ScanSettings class is meant to be the basis
+of a site-specific implementation.
+
 @author: Kay Kasemir
 """
 import re
@@ -13,7 +16,7 @@ class DeviceSettings(object):
        With callback?
        ...
     """
-    def __init__(self, name, completion=False, readback=True, timeout=0.0, tolerance=0.0):
+    def __init__(self, name, completion=False, readback=False, timeout=0.0, tolerance=0.0):
         """name:       Device name
            completion: True to use completion
            readback:   False to not use a readback,
@@ -60,15 +63,42 @@ class DeviceSettings(object):
 
 
 class ScanSettings(object):
-    """Scan Settings:
-       Various lists of devices and how to treat them in a scan
+    """Scan Settings
+    
+       Site-specific implementation is derived from the ScanSettings class.
+       In its constructor, derived classes can call defineDeviceClass() to
+       define settings for devices that match a certain name.
+       
+       In addition, derived class may override computeReadbackName() to
+       provide for example the *.RBV for a motor PV.
     """
 
     def __init__(self):
-        # List that holds DeviceSettings, but using their 'name' as a regular expression pattern.
+        """Derived class can override constructor
+           to add calls to defineDeviceClass()
+        """
+        # List that holds DeviceSettings, but NOT exactly as it's passed out to
+        # uses of this class:
+        # The device_settings[].name is a regular expression pattern for device names.
         self.device_settings = list()
         
-    def defineDeviceClass(self, name_pattern, completion=False, readback=True, timeout=0.0, tolerance=0.0):
+        # In derived class, may register special behavior for certain devices
+        # self.defineDeviceClass("My:Motor.*", completion=True, readback=True, timeout=100)
+
+    def getReadbackName(self, device_name):
+        """device_name: Primary device name
+           Returns the corresponding device name for readback check
+           
+           Derived class may override to compute the readback name
+        """
+        
+        # Example for derived class:
+        #if "Motor" in device_name:
+        #    return device_name + ".RBV"
+        
+        return device_name
+        
+    def defineDeviceClass(self, name_pattern, completion=False, readback=False, timeout=0.0, tolerance=0.0):
         """name_pattern: Device name pattern (regular expression)
            completion:   True to use completion
            readback:     False to not use a readback,
@@ -77,58 +107,68 @@ class ScanSettings(object):
            timeout:      Time out for callback and readback in seconds. 0 to wait forever.
            tolerance:    Tolerance for numeric readback comparison. 
         """
-        self.device_settings.append(DeviceSettings(name_pattern, completion, readback, timeout, tolerance))
+        self.device_settings.append(DeviceSettings(name_pattern, completion, readback, timeout, tolerance))                
                         
-    def getDeviceSettings(self, name):
+    def getDefaultSettings(self, name):
+        """name: Name of device
+           Returns suggested DeviceSettings for that device.
+        """
         for setting in self.device_settings:
             if re.match(setting.getName(), name):
-                return DeviceSettings(name, setting.getCompletion(), setting.getReadback(), setting.getTimeout(), setting.getTolerance())
+                # rb = False (no readback), True (use device name), or "SomeExactName" 
+                rb = setting.readback
+                if rb == True:
+                    rb = self.getReadbackName(name)
+                return DeviceSettings(name, setting.getCompletion(), rb, setting.getTimeout(), setting.getTolerance())
         return DeviceSettings(name)
     
-
-    def parseDeviceModifiers(self, input):
-        """Parse modifiers from device info:
-           -r: No readback
-           -c: No completion
-           +c: Use completion
-           +p: parallel
+    def parseDeviceSettings(self, prefixed_device):
+        """Parse a device name that may be prefixed with modifiers
+           that override the default settings for the device.
+           
+           Supported modifiers:
+           -c: Do not await completion
+           +c: Do await completion
+           -r: Do not check readback
+           +r: Do check readback
+           +p: Access in parallel
         
-           @return: (actual device, completion-or-None, readback-or-None, timeout-or-None,
-                     tolerance-or-None, parallel)
+           @return: ( DeviceSettings(name), parallel )
         """
-        mod_device = input.strip()
+        mod_device = prefixed_device.strip()
         readback = None
         completion = None
-        parallel = None
+        parallel = False
         
         while mod_device.startswith('+') or mod_device.startswith('-'):
-            set = mod_device.startswith('+')
+            yesno = mod_device.startswith('+')
             mod_device = mod_device[1:]
             # one or more modifiers may follow the +|-
             while not mod_device[0] in " +-":
                 if mod_device[0] == 'r':
-                    readback = set
+                    readback = yesno
                 elif mod_device[0] == 'c':
-                    completion = set
+                    completion = yesno
                 elif mod_device[0] == 'p':
-                    parallel = set
+                    parallel = yesno
                 else:
-                    raise Exception("Unknown device modifier %s in %s" % (mod_device[0], input))
+                    raise Exception("Unknown device modifier %s in %s" % (mod_device[0], prefixed_device))
                 mod_device = mod_device[1:]
             mod_device = mod_device.strip()
        
         device = mod_device
 
-        ( def_readback, def_completion, timeout, tolerance ) = self.getDefaults(device)
+        default = self.getDefaultSettings(device)
+
         # Use defaults unless modifiers were provided
-        if def_readback  and readback is None:
-            readback = def_readback
+        if default.getReadback()  and readback is None:
+            readback = default.getReadback()
         if completion is None:
-            completion = def_completion
+            completion = default.getCompletion()
 
         # Turn "do use readback" into the device to use,
         # (def_readback would already provide the device)           
         if readback == True:
             readback = device
         
-        return (device, completion, readback, timeout, tolerance, parallel)
+        return ( DeviceSettings(device, completion, readback, default.getTimeout(), default.getTolerance()), parallel )
