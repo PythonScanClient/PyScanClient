f3c45e1fbc8bb7a4de619fbd958d61e1736ab190
kasemir <kasemirk@ornl.gov>
2015-03-24 10:04:39 -0400

    Update test_commands to work with updated XML generation

--- Test/test_commands.py
+++ Test/test_commands.py
@@ -1,5 +1,6 @@
 
 import unittest
+import xml.etree.ElementTree as ET
 from scan.commands import *
 
 # These tests compare the XML as strings, even though for example
@@ -12,39 +13,39 @@ class CommandTest(unittest.TestCase):
         # Basic comment
         cmd = Comment("Hello")
         print cmd
-        self.assertEqual(cmd.genXML(), "<comment><text>Hello</text></comment>")
+        self.assertEqual(ET.tostring(cmd.genXML()), "<comment><text>Hello</text></comment>")
 
         # Check proper escape of "less than"
         cmd = Comment("Check for current < 10")
         print cmd
-        self.assertEqual(cmd.genXML(), "<comment><text>Check for current &lt; 10</text></comment>")
+        self.assertEqual(ET.tostring(cmd.genXML()), "<comment><text>Check for current &lt; 10</text></comment>")
 
 
     def testSetCommand(self):
         # Basic set
         cmd = Set("some_device", 3.14)
         print cmd
-        self.assertEqual(cmd.genXML(), "<set><device>some_device</device><value>3.14</value></set>")
+        self.assertEqual(ET.tostring(cmd.genXML()), "<set><device>some_device</device><value>3.14</value></set>")
 
         # With completion
         cmd = Set("some_device", 3.14, completion=True)
         print cmd
-        self.assertEqual(cmd.genXML(), "<set><device>some_device</device><value>3.14</value><completion>true</completion></set>")
+        self.assertEqual(ET.tostring(cmd.genXML()), "<set><device>some_device</device><value>3.14</value><completion>true</completion></set>")
 
         # .. and timeout
         cmd = Set("some_device", 3.14, completion=True, timeout=5.0)
         print cmd
-        self.assertEqual(cmd.genXML(), "<set><device>some_device</device><value>3.14</value><completion>true</completion><timeout>5.0</timeout></set>")
+        self.assertEqual(ET.tostring(cmd.genXML()), "<set><device>some_device</device><value>3.14</value><completion>true</completion><timeout>5.0</timeout></set>")
 
         # Setting a readback PV (the default one) enables wait-on-readback
         cmd = Set("some_device", 3.14, completion=True, readback=True, tolerance=1, timeout=10.0)
         print cmd
-        self.assertEqual(cmd.genXML(), "<set><device>some_device</device><value>3.14</value><completion>true</completion><wait>true</wait><readback>some_device</readback><tolerance>1</tolerance><timeout>10.0</timeout></set>")
+        self.assertEqual(ET.tostring(cmd.genXML()), "<set><device>some_device</device><value>3.14</value><completion>true</completion><wait>true</wait><readback>some_device</readback><tolerance>1</tolerance><timeout>10.0</timeout></set>")
 
         # Setting a readback PV (a specific one) enables wait-on-readback
         cmd = Set("some_device", 3.14, completion=True, readback="some_device.RBV", tolerance=1, timeout=10.0)
         print cmd
-        self.assertEqual(cmd.genXML(), "<set><device>some_device</device><value>3.14</value><completion>true</completion><wait>true</wait><readback>some_device.RBV</readback><tolerance>1</tolerance><timeout>10.0</timeout></set>")
+        self.assertEqual(ET.tostring(cmd.genXML()), "<set><device>some_device</device><value>3.14</value><completion>true</completion><wait>true</wait><readback>some_device.RBV</readback><tolerance>1</tolerance><timeout>10.0</timeout></set>")
 
 
 if __name__ == "__main__":
