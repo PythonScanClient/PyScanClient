fb172bf44f87c5e0c5ecab89d78168277f3caaba
kasemir <kasemirk@ornl.gov>
2015-06-08 13:59:37 -0400

    Add version info

---
+++ scan/version.py
@@ -0,0 +1,13 @@
+
+__version__ = '1.0.1'
+
+version_history = """
+1.0.1 - Fix: Set & Loop command set <wait>false</wait> when no readback desired
+
+1.0.0 - Initial Release
+"""
+
+if __name__ == "__main__":
+    print "Version ", __version__
+    
+    print version_history
