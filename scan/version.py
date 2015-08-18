1f7f1afd64281730023c56813b24c91e63ce5454
kasemir <kay.kasemir@gmail.com>
2015-08-18 13:31:02 -0400

    Update version.py

--- scan/version.py
+++ scan/version.py
@@ -1,7 +1,8 @@
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 
 version_history = """
+1.0.4 - Gnumeric support much faster and lower memory
 1.0.3 - Add getDevice() to Loop, Set, Wait commands
 1.0.2 - Fix: Default tolerance of Loop, Set, Wait commands
 1.0.1 - Fix: Set & Loop command set <wait>false</wait> when no readback desired
