800dfbdf97796d75278bd747d83eb3226ed61ba5
Kay Kasemir <kay.kasemir@gmail.com>
2017-09-11 10:48:58 -0400

    Include version detail in doc

---
+++ doc/make_version.py
@@ -0,0 +1,19 @@
+
+print """
+Version Info
+============
+
+To obtain version info::
+
+
+   from scan.version import __version__, version_history
+   print __version__
+   print version_history
+"""
+
+from scan.version import __version__, version_history
+
+print "Version history::"
+
+for line in version_history.splitlines():
+    print ("   " + line)
--- doc/version.rst
+++
@@ -1,9 +0,0 @@
-Version Info
-============
-
-To obtain version info::
-
-
-   from scan.version import __version__, version_history
-   print __version__
-   print version_history
